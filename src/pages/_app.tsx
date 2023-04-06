49cf1ce493256e7a3e4aa67428626751adcf3f89
DmitriyBartenev <bartenevdmitriy@bk.ru>
2023-04-06 19:50:24 +0400

    Profile Page with SSG helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { type AppType } from "next/app";
-
+import Head from "next/head";
 import { api } from "~/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
@@ -9,6 +9,11 @@ import "~/styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>T3 Stack</title>
+        <meta name="description" content="T3 Stack Application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster />
       <Component {...pageProps} />
     </ClerkProvider>
